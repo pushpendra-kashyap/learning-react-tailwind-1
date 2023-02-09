2eff5528cd809cf20f73e5403851b4f7b5b5b676
Dheeraj <dheerajmaurya152002@gmail.com>
2023-02-09 08:08:56 +0530

    navbar with events

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="h-20 bg-red-800 flex justify-between items-center px-10">
@@ -50,44 +50,54 @@ function Navbar() {
       {/* ------- Mobile Slider --------- */}
       {isOpen && (
         <div>
-          <div className="md:hidden fixed right-0 top-0 bottom-0 h-screen bg-red-300 bg-opacity-50 w-52">
-            <div className="h-20 pr-10 flex justify-end items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 text-white cursor-pointer"
-                onClick={() => setIsOpen(false)}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </div>
-            <div className="my-5 flex flex-col gap-5">
-              <a href="/#" className="text-center">
-                Home
-              </a>
-              <a href="/#" className="text-center">
-                About
-              </a>
-              <a href="/#" className="text-center">
-                Skills
-              </a>
-              <a href="/#" className="text-center">
-                Project
-              </a>
-              <div className="flex justify-center">
-                <a
-                  href="/#"
-                  className="bg-red-500 text-white px-3 py-1.5 rounded-md"
-                >
-                  Contact me
-                </a>
+          <div
+            id="container"
+            className="md:hidden fixed inset-0 h-screen bg-gray-500 bg-opacity-60 w-full"
+            onClick={(element) => {
+              if (element.target.id === "container") setIsOpen(false);
+            }}
+          >
+            <div className="flex justify-end w-52 h-full bg-red-300 bg-opacity-50 ">
+              <div className="w-full">
+                <div className="h-20 pr-10 flex justify-end items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-white cursor-pointer"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
+                <div className="my-5 flex flex-col gap-5">
+                  <a href="/#" className="text-center">
+                    Home
+                  </a>
+                  <a href="/#" className="text-center">
+                    About
+                  </a>
+                  <a href="/#" className="text-center">
+                    Skills
+                  </a>
+                  <a href="/#" className="text-center">
+                    Project
+                  </a>
+                  <div className="flex justify-center">
+                    <a
+                      href="/#"
+                      className="bg-red-500 text-white px-3 py-1.5 rounded-md"
+                    >
+                      Contact me
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
