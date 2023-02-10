24aeaa15744a34f602d06481ef3aa5d5e7309a8d
Pushpendra <pushpendrakumarkashyap0@gmail.com>
2023-02-10 08:49:29 +0530

    hero section created

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>Home Page</div>
-  )
+    <div>
+      <div className="relative h-screen sm:h-[550px] ">
+        <img
+          src="https://dlearnerblog.netlify.app/_nuxt/img/homeImage.d05f027.jpeg"
+          alt="hero"
+          className="h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 flex justify-center items-center h-full w-full bg-black bg-opacity-50">
+          <div className="-mt-20 sm:mt-0">
+            <h3 className="text-3xl sm:text-5xl font-bold text-white">
+              DLearner
+            </h3>
+            <p className="text-xl sm:text-2xl font-semibold text-white">
+              you need to learn right now
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
